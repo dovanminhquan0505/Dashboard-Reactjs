109a7012b6a591ccec569468f2a7a95fb51f5efd
Đỗ Văn Minh Quân <minhquandovan@gmail.com>
2024-11-05 17:05:41 +0700

    Update and style for Dashboard sidebar #2

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 import Button from '@mui/material/Button';
 import { MdDashboard } from "react-icons/md";
 import { FaAngleRight } from "react-icons/fa6";
+import { MdOutlineProductionQuantityLimits } from "react-icons/md";
+import { FaShoppingCart } from "react-icons/fa";
+import { AiFillMessage } from "react-icons/ai";
+import { IoIosNotifications } from "react-icons/io";
+import { IoMdSettings } from "react-icons/io";
+import { Link } from 'react-router-dom';
 
 const Sidebar = () => {
   return (
@@ -9,9 +15,98 @@ const Sidebar = () => {
         <div className='sidebar'>
             <ul>
                 <li>
+                    <Link to="/" >
+                        <Button className='w-100'>
+                            <span className='icon'> <MdDashboard /></span>
+                            Dashboard
+                            <span className='arrow'> <FaAngleRight /></span>
+                        </Button>
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/" >
+                        <Button className='w-100'>
+                            <span className='icon'> <MdOutlineProductionQuantityLimits /></span>
+                            Products
+                            <span className='arrow'> <FaAngleRight /></span>
+                        </Button>
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/" >
+                        <Button className='w-100'>
+                            <span className='icon'> <FaShoppingCart /></span>
+                            Orders
+                            <span className='arrow'> <FaAngleRight /></span>
+                        </Button>
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/" >
+                        <Button className='w-100'>
+                            <span className='icon'> <AiFillMessage /></span>
+                            Messages
+                            <span className='arrow'> <FaAngleRight /></span>
+                        </Button>
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/" >
+                        <Button className='w-100'>
+                            <span className='icon'> <IoIosNotifications /></span>
+                            Notifications
+                            <span className='arrow'> <FaAngleRight /></span>
+                        </Button>
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/" >
+                        <Button className='w-100'>
+                            <span className='icon'> <IoMdSettings /></span>
+                            Settings
+                            <span className='arrow'> <FaAngleRight /></span>
+                        </Button>
+                    </Link>
+                </li>
+                <li>
                     <Button className='w-100'>
                         <span className='icon'> <MdDashboard /></span>
                         Dashboard
+                        <span className='arrow'> <FaAngleRight /></span>
+                    </Button>
+                </li>
+                <li>
+                    <Button className='w-100'>
+                        <span className='icon'> <MdOutlineProductionQuantityLimits /></span>
+                        Products
+                        <span className='arrow'> <FaAngleRight /></span>
+                    </Button>
+                </li>
+                <li>
+                    <Button className='w-100'>
+                        <span className='icon'> <FaShoppingCart /></span>
+                        Orders
+                        <span className='arrow'> <FaAngleRight /></span>
+                    </Button>
+                </li>
+                <li>
+                    <Button className='w-100'>
+                        <span className='icon'> <AiFillMessage /></span>
+                        Messages
+                        <span className='arrow'> <FaAngleRight /></span>
+                    </Button>
+                </li>
+                <li>
+                    <Button className='w-100'>
+                        <span className='icon'> <IoIosNotifications /></span>
+                        Notifications
+                        <span className='arrow'> <FaAngleRight /></span>
+                    </Button>
+                </li>
+                <li>
+                    <Button className='w-100'>
+                        <span className='icon'> <IoMdSettings /></span>
+                        Settings
                         <span className='arrow'> <FaAngleRight /></span>
                     </Button>
                 </li>

4b25a62e178b81bf5f126ccadc29a3aac27d8b0c
Đỗ Văn Minh Quân <minhquandovan@gmail.com>
2024-11-09 19:40:12 +0700

    Fix bug icon for sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import Button from '@mui/material/Button';
 import { MdDashboard } from "react-icons/md";
 import { FaAngleRight } from "react-icons/fa6";
-import { MdOutlineProductionQuantityLimits } from "react-icons/md";
+import { FaProductHunt } from "react-icons/fa";
 import { FaShoppingCart } from "react-icons/fa";
 import { AiFillMessage } from "react-icons/ai";
 import { IoIosNotifications } from "react-icons/io";
@@ -11,32 +11,38 @@ import { Link } from 'react-router-dom';
 import { IoLogOut } from "react-icons/io5";
 
 const Sidebar = () => {
-    const [isSubmenuOpen, setIsSubmenuOpen] = useState(false);
+    const [activeTab, setActiveTab] = useState(0);
+    const [isToggleSubmenu, setIsToggleSubmenu] = useState(false);
 
-    const toggleSubmenu = () => {
-        setIsSubmenuOpen(!isSubmenuOpen);
-    }
+    const isOpenSubmenu = (index) => {
+        setActiveTab(index);
+        setIsToggleSubmenu(!isToggleSubmenu);
+    };
 
   return (
     <>
         <div className='sidebar'>
             <ul>
-                <li>
-                    <Link to="/" >
-                        <Button className='w-100'>
-                            <span className='icon'> <MdDashboard /></span>
-                            Dashboard
-                            <span className='arrow'> <FaAngleRight /></span>
+                <Link to="/">
+                    <li>
+                        <Button className={`w-100 ${activeTab === 0 ? 'active' : ''}`} 
+                            onClick={() => isOpenSubmenu(0)} 
+                        >
+                            <span className='icon'><MdDashboard /></span>
+                                Dashboard
+                            <span className='arrow'><FaAngleRight /></span>
                         </Button>
-                    </Link>
-                </li>
-                <li className={isSubmenuOpen ? 'submenu-open' : ''}>
-                    <Button className='w-100' onClick={toggleSubmenu}>
-                        <span className='icon'> <MdOutlineProductionQuantityLimits /></span>
+                    </li>
+                </Link>
+                <li>
+                    <Button className={`w-100 ${activeTab === 1 && isToggleSubmenu === true ? 'active' : ''}`} 
+                        onClick={() => isOpenSubmenu(1)} 
+                    >
+                        <span className='icon'><FaProductHunt /></span>
                         Products
-                        <span className={`arrow ${isSubmenuOpen ? 'rotated' : ''}`}> <FaAngleRight /></span>
+                        <span className="arrow"> <FaAngleRight /></span>
                     </Button>
-                    <div className={`subMenuWrapper ${isSubmenuOpen ? 'open' : ''}`}>
+                    <div className={`subMenuWrapper ${activeTab === 1  && isToggleSubmenu === true ? 'colapse' : 'colapsed'}`}>
                         <ul className="submenu">
                             <li>
                                 <Link to="#" >Product List</Link>
@@ -52,7 +58,8 @@ const Sidebar = () => {
                 </li>
                 <li>
                     <Link to="/" >
-                        <Button className='w-100'>
+                        <Button className={`w-100 ${activeTab === 2 ? 'active' : ''}`}
+                        onClick={() => isOpenSubmenu(2)}>
                             <span className='icon'> <FaShoppingCart /></span>
                             Orders
                             <span className='arrow'> <FaAngleRight /></span>
@@ -61,7 +68,8 @@ const Sidebar = () => {
                 </li>
                 <li>
                     <Link to="/" >
-                        <Button className='w-100'>
+                        <Button className={`w-100 ${activeTab === 3 ? 'active' : ''}`}
+                        onClick={() => isOpenSubmenu(3)}>
                             <span className='icon'> <AiFillMessage /></span>
                             Messages
                             <span className='arrow'> <FaAngleRight /></span>
@@ -70,7 +78,8 @@ const Sidebar = () => {
                 </li>
                 <li>
                     <Link to="/" >
-                        <Button className='w-100'>
+                        <Button className={`w-100 ${activeTab === 4 ? 'active' : ''}`}
+                        onClick={() => isOpenSubmenu(4)}>
                             <span className='icon'> <IoIosNotifications /></span>
                             Notifications
                             <span className='arrow'> <FaAngleRight /></span>
@@ -79,7 +88,8 @@ const Sidebar = () => {
                 </li>
                 <li>
                     <Link to="/" >
-                        <Button className='w-100'>
+                        <Button className={`w-100 ${activeTab === 5 ? 'active' : ''}`}
+                        onClick={() => isOpenSubmenu(5)}>
                             <span className='icon'> <IoMdSettings /></span>
                             Settings
                             <span className='arrow'> <FaAngleRight /></span>
@@ -87,42 +97,48 @@ const Sidebar = () => {
                     </Link>
                 </li>
                 <li>
-                    <Button className='w-100'>
+                    <Button className={`w-100 ${activeTab === 6 ? 'active' : ''}`}
+                    onClick={() => isOpenSubmenu(6)}>
                         <span className='icon'> <MdDashboard /></span>
                         Dashboard
                         <span className='arrow'> <FaAngleRight /></span>
                     </Button>
                 </li>
                 <li>
-                    <Button className='w-100'>
-                        <span className='icon'> <MdOutlineProductionQuantityLimits /></span>
+                    <Button className={`w-100 ${activeTab === 7 ? 'active' : ''}`}
+                    onClick={() => isOpenSubmenu(7)}>
+                        <span className='icon'> <FaProductHunt /></span>
                         Products
                         <span className='arrow'> <FaAngleRight /></span>
                     </Button>
                 </li>
                 <li>
-                    <Button className='w-100'>
+                    <Button className={`w-100 ${activeTab === 8 ? 'active' : ''}`}
+                    onClick={() => isOpenSubmenu(8)}>
                         <span className='icon'> <FaShoppingCart /></span>
                         Orders
                         <span className='arrow'> <FaAngleRight /></span>
                     </Button>
                 </li>
                 <li>
-                    <Button className='w-100'>
+                    <Button className={`w-100 ${activeTab === 9 ? 'active' : ''}`}
+                    onClick={() => isOpenSubmenu(9)}>
                         <span className='icon'> <AiFillMessage /></span>
                         Messages
                         <span className='arrow'> <FaAngleRight /></span>
                     </Button>
                 </li>
                 <li>
-                    <Button className='w-100'>
+                    <Button className={`w-100 ${activeTab === 10 ? 'active' : ''}`}
+                    onClick={() => isOpenSubmenu(10)}>
                         <span className='icon'> <IoIosNotifications /></span>
                         Notifications
                         <span className='arrow'> <FaAngleRight /></span>
                     </Button>
                 </li>
                 <li>
-                    <Button className='w-100'>
+                    <Button className={`w-100 ${activeTab === 11 ? 'active' : ''}`}
+                    onClick={() => isOpenSubmenu(11)}>
                         <span className='icon'> <IoMdSettings /></span>
                         Settings
                         <span className='arrow'> <FaAngleRight /></span>

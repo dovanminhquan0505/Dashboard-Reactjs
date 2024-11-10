a22832d2fcc740dd0dd44eb07a51811e501b818b
Đỗ Văn Minh Quân <minhquandovan@gmail.com>
2024-11-10 17:54:47 +0700

    Update UI for statistics dashboard #2

--- src/screens/Dashboard/components/DashboardBox.jsx
+++ src/screens/Dashboard/components/DashboardBox.jsx
@@ -1,24 +1,84 @@
-import React, { useEffect } from "react";
+import React, { useState } from "react";
+import { HiDotsVertical } from "react-icons/hi";
+import Button from '@mui/material/Button';
+import TrendingUpIcon from '@mui/icons-material/TrendingUp';
+import TrendingDownIcon from '@mui/icons-material/TrendingDown';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import { IoIosTimer } from "react-icons/io";
 
 const DashboardBox = (props) => {
+    const ITEM_HEIGHT = 48;
+    const [anchorEl, setAnchorEl] = useState(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
     return (
-        <div
+        <Button
             className="dashboardBox"
             style={{
                 backgroundImage: `linear-gradient(to right, ${props.color?.[0]}, ${props.color?.[1]})`,
             }}
         >
+
+            {
+                props.grow === true ? <span className="chart"><TrendingUpIcon /></span> : <span className="chart"><TrendingDownIcon /></span>
+            }
+
             <div className="d-flex w-100">
                 <div className="col1">
-                    <h4 className="text-white">Total Users</h4>
+                    <h4 className="text-white mb-0">Total Users</h4>
                     <span className="text-white">277</span>
                 </div>
 
                 <div className="ms-auto">
-                    <span className="icon"></span>
+                    <span className="icon">{props.icon}</span>
                 </div>
             </div>
-        </div>
+
+            <div className="d-flex align-items-center w-100 bottomElement">
+                <h6 className="text-white mb-0 mt-0">Last Month</h6>
+                <div className="ms-auto">
+                    <Button className="ms-auto toggleIcon" onClick={handleClick}><HiDotsVertical /></Button>
+                    <Menu
+                        className="dropdown__menu"
+                        id="long-menu"
+                        MenuListProps={{
+                        'aria-labelledby': 'long-button',
+                        }}
+                        anchorEl={anchorEl}
+                        open={open}
+                        onClose={handleClose}
+                        slotProps={{
+                        paper: {
+                            style: {
+                            maxHeight: ITEM_HEIGHT * 4.5,
+                            width: '20ch',
+                            },
+                        },
+                        }}
+                    >
+                        <MenuItem onClick={handleClose}>
+                            <IoIosTimer /> Last Day
+                        </MenuItem>
+                        <MenuItem onClick={handleClose}>
+                            <IoIosTimer /> Last Week
+                        </MenuItem>
+                            <MenuItem onClick={handleClose}>
+                        <IoIosTimer /> Last Month
+                        </MenuItem>
+                        <MenuItem onClick={handleClose}>
+                            <IoIosTimer /> Last Year
+                        </MenuItem>
+                    </Menu>
+                </div>
+            </div>
+        </Button>
     );
 };
 
--- src/screens/Dashboard/index.jsx
+++ src/screens/Dashboard/index.jsx
@@ -1,22 +1,79 @@
-import React from "react";
+import React, { useState } from "react";
 import DashboardBox from "./components/DashboardBox";
+import { FaUserCircle } from "react-icons/fa";
+import { IoCart } from "react-icons/io5";
+import { MdShoppingBag } from "react-icons/md";
+import { BsStars } from "react-icons/bs";
+import { HiDotsVertical } from "react-icons/hi";
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import { IoIosTimer } from "react-icons/io";
 
 const Dashboard = () => {
+    const ITEM_HEIGHT = 48;
+    const [anchorEl, setAnchorEl] = useState(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
     return (
         <>
             <div className="right__content w-100">
                 <div className="row dashboardBoxWrapperRow">
                     <div className="col-md-8">
                         <div className="dashboardBoxWrapper d-flex">
-                            <DashboardBox color={["#1da256", "#48d483"]}/>
-                            <DashboardBox color={["#c012e2", "#eb64fe"]}/>
-                            <DashboardBox color={["#2c78e5", "#60aff5"]}/>
-                            <DashboardBox color={["#e1950e", "#f3cd29"]}/>
+                            <DashboardBox color={["#1da256", "#48d483"]} icon={<FaUserCircle />} grow={true}/>
+                            <DashboardBox color={["#c012e2", "#eb64fe"]} icon={<IoCart />}/>
+                            <DashboardBox color={["#2c78e5", "#60aff5"]} icon={<MdShoppingBag />}/>
+                            <DashboardBox color={["#e1950e", "#f3cd29"]} icon={<BsStars />}/>
                         </div>
                     </div>
 
                     <div className="col-md-4 ps-0">
-                        <div className="box"></div>
+                        <div className="box graphBox">
+                        <div className="d-flex align-items-center w-100 bottomElement">
+                            <h6 className="text-white mb-0 mt-0">Last Month</h6>
+                            <div className="ms-auto">
+                                <Button className="ms-auto toggleIcon" onClick={handleClick}><HiDotsVertical /></Button>
+                                <Menu
+                                    className="dropdown__menu"
+                                    id="long-menu"
+                                    MenuListProps={{
+                                    'aria-labelledby': 'long-button',
+                                    }}
+                                    anchorEl={anchorEl}
+                                    open={open}
+                                    onClose={handleClose}
+                                    slotProps={{
+                                    paper: {
+                                        style: {
+                                        maxHeight: ITEM_HEIGHT * 4.5,
+                                        width: '20ch',
+                                        },
+                                    },
+                                    }}
+                                >
+                                    <MenuItem onClick={handleClose}>
+                                        <IoIosTimer /> Last Day
+                                    </MenuItem>
+                                    <MenuItem onClick={handleClose}>
+                                        <IoIosTimer /> Last Week
+                                    </MenuItem>
+                                        <MenuItem onClick={handleClose}>
+                                        <IoIosTimer /> Last Month
+                                    </MenuItem>
+                                    <MenuItem onClick={handleClose}>
+                                        <IoIosTimer /> Last Year
+                                    </MenuItem>
+                                </Menu>
+                            </div>
+                        </div>
+                        </div>
                     </div>
                 </div>
             </div>
